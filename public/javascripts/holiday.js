2c68ade6be28abbc7b64a6a5123b80bec79789e1
Camille Warnick <crwarnick@gmail.com>
2018-10-30 21:42:21 +0000

    Updated

--- public/javascripts/holiday.js
+++ public/javascripts/holiday.js
@@ -1,7 +1,6 @@
 /*global angular*/
 angular.module("app", [])
-    .controller("appCtrl", function($scope) {
-        
+    .controller("appCtrl", function($scope, $http) {
             $scope.toDos = [];
             $scope.recipes = [];
             $scope.decors = [];
@@ -13,38 +12,96 @@ angular.module("app", [])
                     ToDo: $scope.toDo,
                     Notes: $scope.notes
                 });
-                console.log($scope.toDos);
+                var objData = { ToDo: $scope.toDo, Notes: $scope.notes };
+                console.log(objData);
+                var toDoURL = 'todo';
+                $http({
+                    url: toDoURL,
+                    method: "POST",
+                    data: objData
+                }).success(function(data, status, headers, config) {
+                    console.log("Post worked");
+                }).error(function(data, status, headers, config) {
+                    console.log("Post failed");
+                });
             };
 
             $scope.addRecipe = function() {
                 $scope.recipes.push({
-                    RName: $scope.rName,
-                    RIUrl: $scope.riUrl,
+                    RName: $scope.rName, 
+                    RIUrl: $scope.riUrl, 
                     RUrl: $scope.rUrl
+                });
+                var objData = { RName: $scope.rName, RIUrl: $scope.riUrl, RUrl: $scope.rUrl };
+                console.log(objData);
+                var recipeURL = 'recipe';
+                $http({
+                    url: recipeURL,
+                    method: "POST",
+                    data: objData
+                }).success(function(data, status, headers, config) {
+                    console.log("Post worked");
+                }).error(function(data, status, headers, config) {
+                    console.log("Post failed");
                 });
             };
 
             $scope.addDecor = function() {
                 $scope.decors.push({
-                    DName: $scope.dName,
-                    DIUrl: $scope.diUrl,
+                    DName: $scope.dName, 
+                    DIUrl: $scope.diUrl
+                });
+                var objData = { DName: $scope.dName, DIUrl: $scope.diUrl };
+                console.log(objData);
+                var decorURL = 'decor';
+                $http({
+                    url: decorURL,
+                    method: "POST",
+                    data: objData
+                }).success(function(data, status, headers, config) {
+                    console.log("Post worked");
+                }).error(function(data, status, headers, config) {
+                    console.log("Post failed");
                 });
             };
-
+    
             $scope.addActivity = function() {
                 $scope.activities.push({
-                    AName: $scope.aName,
-                    AIUrl: $scope.aiUrl,
+                    AName: $scope.aName, 
+                    AIUrl: $scope.aiUrl, 
                     AUrl: $scope.aUrl
+                });
+                var objData = { AName: $scope.aName, AIUrl: $scope.aiUrl, AUrl: $scope.aUrl };
+                console.log(objData);
+                var activityURL = 'activity';
+                $http({
+                    url: activityURL,
+                    method: "POST",
+                    data: objData
+                }).success(function(data, status, headers, config) {
+                    console.log("Post worked");
+                }).error(function(data, status, headers, config) {
+                    console.log("Post failed");
                 });
             };
 
             $scope.addPerson = function() {
                 $scope.people.push({
-                    PName: $scope.pName,
-                    PIUrl: $scope.piUrl,
+                    PName: $scope.pName, 
+                    PIUrl: $scope.piUrl, 
                     PNumber: $scope.pNumber
                 });
+                var objData = { PName: $scope.pName, PIUrl: $scope.piUrl, PNumber: $scope.pNumber };
+                console.log(objData);
+                var personURL = 'person';
+                $http({
+                    url: personURL,
+                    method: "POST",
+                    data: objData
+                }).success(function(data, status, headers, config) {
+                    console.log("Post worked");
+                }).error(function(data, status, headers, config) {
+                    console.log("Post failed");
+                });
             };
-
-        });
+    });
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,60 @@ router.get('/', function(req, res) {
     res.sendFile('holiday.html', {root: 'public'});
 });
 
-router.get('/halloween', function(req, res) {
-    var file = fs.readFileSync('routes/arrays.json');
-    var json = JSON.parse(file);
-    res.send(json);
-})
+var toDos = [];
+var recipes = [];
+var decors = [];
+var activities = [];
+var people = [];
+
+router.get('/todo', function(req, res) {
+        res.send(toDos);
+});
+
+router.post('/todo', function(req, res) {
+    console.log(req.body);
+    toDos.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+router.get('/recipe', function(req, res) {
+        res.send(recipes);
+});
+
+router.post('/recipe', function(req, res) {
+    console.log(req.body);
+    recipes.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+router.get('/decor', function(req, res) {
+        res.send(decors);
+});
+
+router.post('/decor', function(req, res) {
+    console.log(req.body);
+    decors.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+router.get('/activity', function(req, res) {
+        res.send(activities);
+});
+
+router.post('/activity', function(req, res) {
+    console.log(req.body);
+    activities.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+
+router.get('/person', function(req, res) {
+        res.send(people);
+});
+
+router.post('/person', function(req, res) {
+    console.log(req.body);
+    people.push(req.body);
+    res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
 
 module.exports = router;
